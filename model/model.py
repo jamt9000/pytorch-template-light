f36da673b927602846fa2b64e42fe518b457f623
James Thewlis <james@unitary.ai>
2021-01-25 00:04:44 +0100

    Delete BaseModel
    I find everything being nn.Module less mysterious

--- model/model.py
+++ model/model.py
@@ -1,15 +1,14 @@
 import torch.nn as nn
 import torch.nn.functional as F
-from base import BaseModel
 
 
-class MnistModel(BaseModel):
+class MnistModel(nn.Module):
     def __init__(self, num_classes=10):
         super().__init__()
         self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
-        self.gap = nn.AdaptiveAvgPool2d((4,4))
+        self.gap = nn.AdaptiveAvgPool2d((4, 4))
         self.fc1 = nn.Linear(320, 50)
         self.fc2 = nn.Linear(50, num_classes)
 
@@ -17,7 +16,7 @@ class MnistModel(BaseModel):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
         x = self.gap(x)
-        x = x.view(-1,320)
+        x = x.view(-1, 320)
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
